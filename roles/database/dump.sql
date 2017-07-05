31b0dc23cb3ca3e3b8aabad634bcfbb19757df02
zetas1982 <antoniojimenezramirez30@gmail.com>
2017-07-05 12:00:08 +0200

    Delete dump.sql

--- roles/database/dump.sql
+++
@@ -1,6 +0,0 @@
-# roles/database/files/dump.sql
-CREATE TABLE IF NOT EXISTS demo1 (
-  message varchar(255) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
-INSERT INTO demo1 (message) VALUES('Hello World!');
