3c74b30ffe189e1fc1b7bf9375cfbaa015a8bee7
zetas1982 <antoniojimenezramirez30@gmail.com>
2017-07-05 14:07:08 +0200

    Update db.php

--- roles/database/files/db.php
+++ roles/database/files/db.php
@@ -1,12 +1,13 @@
 <?php
-$db_username="demo1";
-$db_password="demo1";
-$db_host="localhost";
-$db_name="demo1";
-$link = mysql_connect($db_host, $db_username, $db_password);
-mysql_select_db($db_name, $link);
-$result = mysql_query("SELECT * FROM demo1", $link);
-$row = mysql_fetch_assoc($result);
-print_r($row);
-mysql_close($link);
+#$db_username="demo1";
+#$db_password="demo1";
+#$db_host="localhost";
+#$db_name="demo1";
+#$link = mysql_connect($db_host, $db_username, $db_password);
+#mysql_select_db($db_name, $link);
+#$result = mysql_query("SELECT * FROM demo1", $link);
+#$row = mysql_fetch_assoc($result);
+#print_r($row);
+#mysql_close($link);
+phpinfo();
 ?>
