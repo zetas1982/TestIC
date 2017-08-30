a49fe87eba75c9216fa33b3ac9f3aa26b56d1c53
zetas1982 <antoniojimenezramirez30@gmail.com>
2017-08-30 09:20:44 +0200

    Delete arrancaoracle.sh

--- ovirt/tasks/arrancaoracle.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-HOSTNAME=dlioracle01
-USERNAME=oracle
-SCRIPT="/tss_admin/oracle/oraclectl.sh start tomsci"
-sshpass -p 'JaGbsnkYpWshpCNbnE4o' ssh -l ${USERNAME} ${HOSTNAME} "${SCRIPT}" 
